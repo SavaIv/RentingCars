ca35c22d2d6e72264361eb4bbf0f75825c406618
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2022-12-14 15:32:02 +0200

    Create a project in the directory

---
+++ RentingCars/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentingCars.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
