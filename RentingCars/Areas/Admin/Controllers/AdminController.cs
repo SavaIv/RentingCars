1ffc0bbd6b0ecb46bf5ff22df392ceec58169933
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-02-17 19:19:20 +0200

    Add Admin Area

---
+++ RentingCars/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentingCars.Areas.Admin.Controllers
+{
+    [Area(AdminConstants.AreaName)]
+    [Authorize(Roles = WebConstants.AdministratorRoleName)]
+    public abstract class AdminController : Controller
+    {
+    }
+}
---
+++ RentingCars/Areas/Admin/Controllers/CarsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentingCars.Areas.Admin.Controllers
+{
+    public class CarsController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
