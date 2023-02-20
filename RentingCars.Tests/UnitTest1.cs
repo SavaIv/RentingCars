3481053719a6b3256a6bc170843f44ccfd36779b
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-02-20 19:23:35 +0200

    Adding Test project (xUnit + Moq)

---
+++ RentingCars.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace RentingCars.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("2", 2.ToString());
+        }
+    }
+}
