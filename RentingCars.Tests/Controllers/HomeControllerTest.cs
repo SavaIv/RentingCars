67ee7ae4fc90f1173bfccc3209248c8f1794a14c
Sava Ivanov <59567777+SavaIv@users.noreply.github.com>
2023-02-23 21:08:45 +0200

    Integration Test -> HomeController

--- RentingCars.Tests/Controllers/HomeControllerTest.cs
+++ RentingCars.Tests/Controllers/HomeControllerTest.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RentingCars.Controllers;
+using RentingCars.Data.Models;
+using RentingCars.Models.Home;
+using RentingCars.Services.Cars;
+using RentingCars.Services.Statistics;
+using RentingCars.Tests.Mocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +16,43 @@ namespace RentingCars.Tests.Controllers
 {
     public class HomeControllerTest
     {
-        //[Fact]
+        [Fact]
+        public void IndexShouldReturnViewWithCorrectModel()
+        {
+            // Arange
+            var data = DatabaseMock.Instance;
+            var mapper = MapperMock.Instance;
 
+            data.Cars.AddRange(Enumerable.Range(0, 10).Select(i => new Car()
+            {
+                Brand = "brand",
+                Description = "description",
+                ImageUrl = "imageUrl",
+                Model = "model"                
+            }));
+            data.Users.Add(new User());
+            data.SaveChanges();
+
+            var carService = new CarService(data, mapper);
+            var statisticService = new StatisticsService(data);
+
+            var homeController = new HomeController(carService, statisticService);
+
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            Assert.NotNull(result);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+            var indexViewModel = Assert.IsType<IndexViewModel>(model);
+
+            Assert.Equal(3, indexViewModel.Cars.Count);
+            Assert.Equal(10, indexViewModel.TotalCars);
+            Assert.Equal(1, indexViewModel.TotalUsers);
+        }
 
 
         [Fact]
---
+++ RentingCars.Tests/Mocks/MapperMock.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using RentingCars.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentingCars.Tests.Mocks
+{
+    public static class MapperMock
+    {
+        public static IMapper Instance
+        {
+            get
+            {
+                var mapperConfiguraion = new MapperConfiguration(config =>
+                {
+                    config.AddProfile<MappingProfile>();
+                });
+
+                return new Mapper(mapperConfiguraion);
+            }
+        }
+    }
+}
